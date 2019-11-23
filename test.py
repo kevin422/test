26c3e08b0245817da859c1e70a564a9ac3b87594
Administrator <7631778>
2019-11-23 15:33:41 +0800

    新增test.py文件

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @Time :2019/11/23 15:31
+# @Author:wyj
+# @Email:904509617@qq.com
+# @File: test.py
+
+def run():
+    print("you can run fast.")
+
+if __name__ == '__main__':
+    run()
